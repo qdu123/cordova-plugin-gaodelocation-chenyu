fadf71a161b34f483aaf3a6923ee3e9ed3fb4772
zhulei <ivan.zhu@sfygroup.com>
2019-03-30 14:35:33 +0800

    返回的坐标转成gps坐标

--- src/android/GaoDeLocation.java
+++ src/android/GaoDeLocation.java
@@ -20,6 +20,7 @@ import android.annotation.TargetApi;
 import android.content.pm.PackageManager;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class GaoDeLocation extends CordovaPlugin {
@@ -105,7 +106,7 @@ public class GaoDeLocation extends CordovaPlugin {
     private AMapLocationClientOption getDefaultOption() {
         AMapLocationClientOption mOption = new AMapLocationClientOption();
         mOption.setLocationMode(AMapLocationMode.Hight_Accuracy);//可选，设置定位模式，可选的模式有高精度、仅设备、仅网络。默认为高精度模式
-        mOption.setGpsFirst(false);//可选，设置是否gps优先，只在高精度模式下有效。默认关闭
+        mOption.setGpsFirst(true);//可选，设置是否gps优先，只在高精度模式下有效。默认关闭
         mOption.setHttpTimeOut(30000);//可选，设置网络请求超时时间。默认为30秒。在仅设备模式下无效
         mOption.setInterval(2000);//可选，设置定位间隔。默认为2秒
         mOption.setNeedAddress(true);//可选，设置是否返回逆地理地址信息。默认是true
@@ -130,20 +131,14 @@ public class GaoDeLocation extends CordovaPlugin {
                     //解析定位结果
                     //errCode等于0代表定位成功，其他的为定位失败，具体的可以参照官网定位错误码说明
                     if (location.getErrorCode() == 0) {
+                        HashMap<String, Double> hm = delta(location.getLatitude(),location.getLongitude());
                         json.put("status", "定位成功");
                         //定位类型
                         json.put("type", location.getLocationType());
                         //纬度
-                        json.put("latitude", location.getLatitude());
+                        json.put("latitude", hm.get("lat"));
                         //经度
-                        json.put("longitude", location.getLongitude());
-                        //精度
-                        json.put("accuracy", location.getAccuracy());
-                        //角度
-                        json.put("bearing", location.getBearing());
-                        // 获取当前提供定位服务的卫星个数
-                        //星数
-                        json.put("satellites", location.getSatellites());
+                        json.put("longitude", hm.get("lon"));
                         //国家
                         json.put("country", location.getCountry());
                         //省
@@ -154,12 +149,8 @@ public class GaoDeLocation extends CordovaPlugin {
                         json.put("citycode", location.getCityCode());
                         //区
                         json.put("district", location.getDistrict());
-                        //区域码
-                        json.put("adcode", location.getAdCode());
                         //地址
                         json.put("address", location.getAddress());
-                        //兴趣点
-                        json.put("poi", location.getPoiName());
                         //兴趣点
                         json.put("time", location.getTime());
                     } else {
@@ -280,5 +271,49 @@ public class GaoDeLocation extends CordovaPlugin {
         if (requestCode == PERMISSION_REQUEST_CODE) {
             this.getCurrentPosition();
         }
+    }
+
+    //圆周率 GCJ_02_To_WGS_84
+    double PI = 3.14159265358979324;
+    /**
+     * @author 作者:
+     * 方法描述:方法可以将高德地图SDK获取到的GPS经纬度转换为真实的经纬度，可以用于解决安卓系统使用高德SDK获取经纬度的转换问题。
+     * @param 需要转换的经纬度
+     * @return 转换为真实GPS坐标后的经纬度
+     * @throws <异常类型> {@inheritDoc} 异常描述
+     */
+    public HashMap<String, Double> delta(double lat,double lon) {
+        double a = 6378245.0;//克拉索夫斯基椭球参数长半轴a
+        double ee = 0.00669342162296594323;//克拉索夫斯基椭球参数第一偏心率平方
+        double dLat = this.transformLat(lon - 105.0, lat - 35.0);
+        double dLon = this.transformLon(lon - 105.0, lat - 35.0);
+        double radLat = lat / 180.0 * this.PI;
+        double magic = Math.sin(radLat);
+        magic = 1 - ee * magic * magic;
+        double sqrtMagic = Math.sqrt(magic);
+        dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * this.PI);
+        dLon = (dLon * 180.0) / (a / sqrtMagic * Math.cos(radLat) * this.PI);
+
+        HashMap<String, Double> hm = new HashMap<String, Double>();
+        hm.put("lat",lat - dLat);
+        hm.put("lon",lon - dLon);
+
+        return hm;
+    }
+    //转换经度
+    public double transformLon(double x, double y) {
+        double ret = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * Math.sqrt(Math.abs(x));
+        ret += (20.0 * Math.sin(6.0 * x * this.PI) + 20.0 * Math.sin(2.0 * x * this.PI)) * 2.0 / 3.0;
+        ret += (20.0 * Math.sin(x * this.PI) + 40.0 * Math.sin(x / 3.0 * this.PI)) * 2.0 / 3.0;
+        ret += (150.0 * Math.sin(x / 12.0 * this.PI) + 300.0 * Math.sin(x / 30.0 * this.PI)) * 2.0 / 3.0;
+        return ret;
+    }
+    //转换纬度
+    public double transformLat(double x, double y) {
+        double ret = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * Math.sqrt(Math.abs(x));
+        ret += (20.0 * Math.sin(6.0 * x * this.PI) + 20.0 * Math.sin(2.0 * x * this.PI)) * 2.0 / 3.0;
+        ret += (20.0 * Math.sin(y * this.PI) + 40.0 * Math.sin(y / 3.0 * this.PI)) * 2.0 / 3.0;
+        ret += (160.0 * Math.sin(y / 12.0 * this.PI) + 320 * Math.sin(y * this.PI / 30.0)) * 2.0 / 3.0;
+        return ret;
     }
 }
--- src/ios/GaoDeLocation.m
+++ src/ios/GaoDeLocation.m
@@ -4,8 +4,11 @@
 #import <AMapFoundationKit/AMapFoundationKit.h>
 
 #import <AMapLocationKit/AMapLocationKit.h>
+#include <math.h>
 @interface GaoDeLocation : CDVPlugin {
     // Member variables go here.
+    //圆周率 GCJ_02_To_WGS_84
+    double PI ;
 }
 
 @property(nonatomic,strong)NSString *IOS_API_KEY;
@@ -18,7 +21,7 @@
 -(void)pluginInitialize
 {
     self.IOS_API_KEY = [[self.commandDelegate settings] objectForKey:@"ios_api_key"];
-    
+    PI= 3.14159265358979324;
     [AMapServices sharedServices].apiKey =self.IOS_API_KEY;
     // 带逆地理信息的一次定位（返回坐标和地址信息）
     [self configLocationManager];
@@ -65,10 +68,12 @@
         if (regeocode)
         {
             NSMutableDictionary *mDict = [[NSMutableDictionary alloc] init];
+            //转成标准gps坐标
+            NSDictionary *gpsDic=[self deltaWithLatAndLng:location.coordinate.latitude lon:location.coordinate.longitude];
             [mDict setObject:@"定位成功" forKey:@"status"];
             [mDict setObject:@"" forKey:@"type"];
-            [mDict setObject:[NSNumber numberWithDouble:location.coordinate.latitude]  forKey:@"latitude"];
-            [mDict setObject:[NSNumber numberWithDouble:location.coordinate.longitude] forKey:@"longitude"];
+            [mDict setObject:[gpsDic objectForKey:@"lat"] forKey:@"latitude"];
+            [mDict setObject:[gpsDic objectForKey:@"lon"] forKey:@"longitude"];
             [mDict setObject:[NSString stringWithFormat:@"%g",location.horizontalAccuracy] forKey:@"accuracy"];
             //        [mDict setObject:[NSString stringWithFormat:@"%g",location.bearing] forKey:@"bearing"];
             //        [mDict setObject:@"one2" forKey:@"satellites"];
@@ -99,5 +104,50 @@
     NSLog(@"message = %@",message);
     CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:message];
     [self.commandDelegate sendPluginResult:commandResult callbackId:callbackID];
+}
+
+
+/**
+ * @author 作者:
+ * 方法描述:方法可以将高德地图SDK获取到的GPS经纬度转换为真实的经纬度
+ * @param 需要转换的经纬度
+ * @return 转换为真实GPS坐标后的经纬度
+ * @throws <异常类型> {@inheritDoc} 异常描述
+ */
+-(NSDictionary*) deltaWithLatAndLng:  (double) lat
+                  lon: (double) lon {
+    double a = 6378245.0;//克拉索夫斯基椭球参数长半轴a
+    double ee = 0.00669342162296594323;//克拉索夫斯基椭球参数第一偏心率平方
+    double dLat = [self transformLat:lon - 105.0 y: lat - 35.0];
+    double dLon = [self transformLon:lon - 105.0 y:lat - 35.0 ];
+    double radLat = lat / 180.0 * PI;
+    double magic = sin(radLat);
+    magic = 1 - ee * magic * magic;
+    double sqrtMagic = sqrt(magic);
+    dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * PI);
+    dLon = (dLon * 180.0) / (a / sqrtMagic * cos(radLat) * PI);
+    NSDictionary *dic= [[NSDictionary alloc]initWithObjectsAndKeys:[NSNumber    numberWithDouble:lat - dLat] , @"lat",
+                        [NSNumber numberWithDouble:lon - dLon] , @"lon",nil];
+
+    
+    return dic;
+}
+//转换经度
+-(double) transformLon: (double) x
+    y:(double) y {
+    double ret = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * sqrt(abs(x));
+    ret += (20.0 * sin(6.0 * x * PI) + 20.0 * sin(2.0 * x * PI)) * 2.0 / 3.0;
+    ret += (20.0 * sin(x * PI) + 40.0 * sin(x / 3.0 * PI)) * 2.0 / 3.0;
+    ret += (150.0 * sin(x / 12.0 * PI) + 300.0 * sin(x / 30.0 * PI)) * 2.0 / 3.0;
+    return ret;
+}
+//转换纬度
+-(double) transformLat: (double) x
+                     y:(double) y {
+    double ret = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * sqrt(abs(x));
+    ret += (20.0 * sin(6.0 * x * PI) + 20.0 *sin(2.0 * x * PI)) * 2.0 / 3.0;
+    ret += (20.0 * sin(y * PI) + 40.0 * sin(y / 3.0 * PI)) * 2.0 / 3.0;
+    ret += (160.0 * sin(y / 12.0 * PI) + 320 * sin(y * PI / 30.0)) * 2.0 / 3.0;
+    return ret;
 }
 @end
